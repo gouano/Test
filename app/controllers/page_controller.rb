750f95bcc84542eb62376a1f897811e79d6747ac
Vipette Ibano <pet.ibano30@gmail.com>
2018-02-18 01:55:04 +0800

    Add about and contact routes;
    Signed-off-by: Vipette Ibano <pet.ibano30@gmail.com>

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,9 +1,23 @@
 class PageController < ApplicationController
 
-  # GET /
+  # GET '/'
   def home
     respond_to do |format|
       format.all { render :plain => "Food Quest" }
+    end
+  end
+
+  # GET '/about'
+  def about
+    respond_to do |format|
+      format.all { render :plain => "Food Quest | About" }
+    end
+  end
+
+  # GET '/contact'
+  def contact
+    respond_to do |format|
+      format.all { render :plain => "Food Quest | Contact" }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-
   root 'page#home'
+
+  match '/about',     to: 'page#about',     via: [:get]
+  match '/contact',   to: 'page#contact',   via: [:get, :post]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
